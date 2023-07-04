f07c0498e7ef8ff24b06051e552402d10f24487a
mannuXD <90824516+kumarmanish21@users.noreply.github.com>
2023-07-04 11:03:52 +0530

    Update start_help.py

--- Adarsh/bot/plugins/start_help.py
+++ Adarsh/bot/plugins/start_help.py
@@ -77,7 +77,7 @@ async def start(b, m):
     await StreamBot.send_photo(
         chat_id=m.chat.id,
         photo ="https://telegra.ph/file/19eeb26fa2ce58765917a.jpg",
-        caption =f'Hi {m.from_user.mention(style="md")}!,\nI am Telegram File to Link Generator Bot with Channel support.\nSend me any file and get a direct download link and streamable link.!',
+        caption =f'Hi {m.from_user.mention(style="md")}!,\nI am Telegram File to Link Generator Bot with Channel support. Join➡️@Girlfriend4u_Official \nSend me any file and get a direct download link and streamable link.!',
         reply_markup=buttonz)
 
 
